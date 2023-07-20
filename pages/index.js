034a018b9b8fbe817be2da8cfc719a8a33bd9d8b
Dipu Sharma <sdipu231@gmail.com>
2023-07-20 09:36:27 +0530

    slider-and-css-added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import HeroBanner from "@/components/HeroBanner"
 export default function Home() {
   return (
-    <main className="h-[1000px]">
+    <main className="">
       <HeroBanner/>
     </main>
   )
